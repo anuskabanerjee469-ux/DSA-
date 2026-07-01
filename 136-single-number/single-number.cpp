626dc1b17e946bbd42f8b0d3c9af199a2b0439bb
anuskabanerjee469-ux <anuskabanerjee469@gmail.com>
2026-07-01 14:02:51 +0530

    Time: 24 ms (5.59%) | Memory: 24.5 MB (7.50%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,12 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0 ;
-        for(int val: nums ){
-            ans = ans^val;
-        }
-        return ans;
-        }
-        
+        map<int, int> mp;
 
-    };
+        // Count frequency of each element
+        for (int num : nums) {
+            mp[num]++;
+        }
+
+        // Find the element that appears only once
+        for (auto it : mp) {
+            if (it.second == 1) {
+                return it.first;
+            }
+        }
+
+        return -1; // This line will never be reached for valid input
+    }
+};
