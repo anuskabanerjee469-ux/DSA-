acaf300f07dd770b140ca790c37fef409e260ca0
anuskabanerjee469-ux <anuskabanerjee469@gmail.com>
2026-07-07 22:38:39 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (32.41%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-      int candidate = 0;
+        int candidate = 0;
         int count = 0;
 
         for (int num : nums) {
-            if (count == 0)
+            if (count == 0) {
                 candidate = num;
+            }
 
-            count += (num == candidate) ? 1 : -1;
+            if (num == candidate)
+                count++;
+            else
+                count--;
         }
 
         return candidate;
