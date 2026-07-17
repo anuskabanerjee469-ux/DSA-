b2c960c912deb94d1490df42c5f51757027312b1
anuskabanerjee469-ux <anuskabanerjee469@gmail.com>
2026-07-17 10:33:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (54.43%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,15 +1,58 @@
 class Solution {
 public:
+
+    // Function to find the first occurrence
+    int findFirst(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;          // Store answer
+                high = mid - 1;     // Search on left side
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    // Function to find the last occurrence
+    int findLast(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;          // Store answer
+                low = mid + 1;      // Search on right side
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
     vector<int> searchRange(vector<int>& nums, int target) {
 
-        auto lb = lower_bound(nums.begin(), nums.end(), target);
-        auto ub = upper_bound(nums.begin(), nums.end(), target);
-
-        if (lb == nums.end() || *lb != target)
-            return {-1, -1};
-
-        int first = lb - nums.begin();
-        int last = ub - nums.begin() - 1;
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
 
         return {first, last};
     }
